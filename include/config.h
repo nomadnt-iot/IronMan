27be49f2751257c016bde3d4ca40ed02245dd0b0
Filippo Sallemi <fsallemi@nomadnt.com>
2021-09-18 16:47:15 +0200

    added comments

--- include/config.h
+++ include/config.h
@@ -1,7 +1,6 @@
-#define LED_COUNT 12
-#define LED_PIN 4
-#define LED_BRIGHTNESS 10
-#define LED_HUE 0
+#define LED_COUNT 12        // number of leds present on your board
+#define LED_PIN 4           // pin to be used to manage the leds
+#define LED_BRIGHTNESS 10   // the default starting brightness
 
-#define BTN_PIN 2
-#define BTN_DEBOUNCE 50
+#define BTN_PIN 2           // pin to be used for button
+#define BTN_DEBOUNCE 50     // button debounce
